18f9fabe5a552a021d0233ed799c53e44be0b5c2
blackhawk99 <37162571+blackhawk99@users.noreply.github.com>
2025-04-06 23:54:46 +0300

    Add files via upload
    99

---
+++ src/data/greece-coastline.js
@@ -0,0 +1,39 @@
+export const greeceCoastline = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          // Athens Riviera section
+          [23.7686, 37.8207], // Kavouri
+          [23.7761, 37.8235], 
+          [23.7850, 37.8095], // Astir Beach area
+          [23.7938, 37.8024],
+          [23.8011, 37.8133], // Varkiza
+          [23.7808, 37.8179], // Vouliagmeni
+          [23.7470, 37.8650], // Glyfada
+          [23.6900, 37.9000], // Further north
+          [23.9516, 37.7216], // Agii Anargiri
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [22.9980, 36.1360], // Kapsali
+          [22.9944, 36.1388],
+          [22.9896, 36.1422],
+          [23.0410, 36.2260], // Palaiopoli
+          [23.0380, 36.2310],
+          [23.0330, 36.2350]
+        ]
+      }
+    }
+  ]
+};
