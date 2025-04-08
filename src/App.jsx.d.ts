f1a15a99afd483c0844f0d917113e67f793a9ce9
blackhawk99 <37162571+blackhawk99@users.noreply.github.com>
2025-04-09 00:28:24 +0300

    Update App.jsx.d.ts

--- src/App.jsx.d.ts
+++ src/App.jsx.d.ts
@@ -1,2 +1,6 @@
-// App.jsx.d.ts
-declare module './App.jsx';
+// src/App.jsx.d.ts
+declare module './App.jsx' {
+  import React from 'react';
+  const App: React.ComponentType;
+  export default App;
+}
