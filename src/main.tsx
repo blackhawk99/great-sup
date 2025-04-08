766184411f6dce0c15be84934ac77648ee1f127f
blackhawk99 <37162571+blackhawk99@users.noreply.github.com>
2025-04-09 00:31:11 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// @ts-ignore - Suppress TypeScript error about default export
 import App from "./App.jsx";
 import "./index.css";
 
