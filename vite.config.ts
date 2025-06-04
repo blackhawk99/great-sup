1ee608550e0adff0bf7685ae63cc7054e119fce1
blackhawk99 <37162571+blackhawk99@users.noreply.github.com>
2025-06-04 11:17:34 +0300

    Show last code update date in footer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { execSync } from "child_process";
 
 // https://vitejs.dev/config/
+const lastCommit = execSync("git log -1 --format=%cd").toString().trim();
+
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __LAST_COMMIT_DATE__: JSON.stringify(lastCommit),
+  },
 });
